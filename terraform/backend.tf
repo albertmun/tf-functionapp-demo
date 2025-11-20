b4f3dcf5d372710d596b66b5ce474507fb675a92
Albert <albertmun@microsoft.com>
2025-11-20 12:02:56 -0800

    TEST TRIGGER

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,5 @@
 # Backend configuration for Terraform state
 # This should be created manually or via a separate bootstrap process
-
 terraform {
   backend "azurerm" {
     resource_group_name  = "tf-demo"
